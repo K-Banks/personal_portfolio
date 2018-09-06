547a59e2c886778139b11b81fbd8937daa91a59a
Kayl Eubanks <kayleubanks@gmail.com>
2018-09-06 16:20:46 -0700

    Add script to animate greeting text and tagline

--- src/main.js
+++ src/main.js
@@ -3,5 +3,9 @@ import './css/styles.css';
 
 
 $(document).ready(function(){
-
+  $('#greeting').fadeIn(1500, function () {
+    setTimeout(function() {
+      $('#tagline').fadeIn(1500)
+    }, 500);
+  });
 });
