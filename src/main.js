c4fac7707f3e5ddb6ea3aec6f098d425889ed2f9
Kayl Eubanks <kayleubanks@gmail.com>
2018-09-11 13:10:57 -0700

    Create state array and manipulate header of project info section matching project clicked on

--- src/main.js
+++ src/main.js
@@ -3,6 +3,47 @@ import './css/styles.css';
 
 
 $(document).ready(function(){
+
+  const projectInformation = [
+    {
+      id: "onTheSpotify",
+      title: "On-The-Spotify",
+      about: ["Paragraph 1", "paragraph 2", "paragraph 3"],
+      link: "https://github.com/K-Banks/On-The-Spotify"
+    },
+    {
+      id: "dndSpellApi",
+      title: "DnD Spell API",
+      about: ["Dnd paragraph 1", "Dnd paragraph 2"],
+      link: "https://github.com/K-Banks/dnd-organizer-api"
+    },
+    {
+      id: "personalPortfolio",
+      title: "My Portfolio Site",
+      about: ["portfolio paragraph 1", "portfolio paragraph 2"],
+      link: "https://github.com/K-Banks/personal_portfolio"
+    },
+    {
+      id: "tbd",
+      title: "Undecided Angular Project",
+      about: ["Angular paragraph 1", "Angular paragraph 2", "Angular paragraph 3"],
+      link: "https://github.com/K-Banks/gamasutra-clone"
+    }
+  ];
+
+  function insertProjectInfo(projectId) {
+    let currentProject = null;
+    let i = 0;
+    while (currentProject == null || i < projectInformation.length) {
+      if (projectInformation[i].id == projectId) {
+        currentProject = projectInformation[i];
+      }
+      i += 1;
+    }
+    console.log(currentProject.title);
+    $("#projectTitle h2").text(currentProject.title);
+  }
+
   $('#greeting').fadeIn(1500, function () {
     setTimeout(function() {
       $('#tagline').fadeTo(1500, 1)
@@ -34,6 +75,8 @@ $(document).ready(function(){
   });
 
   $(".projectButton").click(function(event) {
+    let projectId = $(event.target).attr('id');
+    insertProjectInfo(projectId);
     $(".projectButton").removeClass("active");
     $(event.target).addClass("active");
     $("#projectInfo").slideDown(1000);
